f1de92fdbf3bd7fc3af0eb8726e86ff7ee5f7762
yaniva <yaniv.a@simplee.com>
2018-01-11 16:44:09 +0200

    create rails app and set up home and about pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yanivemperorapp::Application.config.secret_token = 'edc502cd931c48c47f39c90da6a04d4b72ff242f2dc4d766a1fbba75a405fc804692d28ab84c8389c3fb1ad164f44699d28682f1df10d04c65cca70da265c2cf'
